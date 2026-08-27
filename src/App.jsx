f4c049a7d3287787b78b01193074f8a65f756a35
jooeunhan <yeppijune@gmail.com>
2026-08-27 09:15:38 +0900

    :sparkles: Feat: 대시보드 페이지 Router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import RootLayout from "./layout/RootLayout";
 import LoginPage from "./pages/Login/LoginPage";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route element={<RootLayout />}>
+          <Route index element={<DashboardPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
