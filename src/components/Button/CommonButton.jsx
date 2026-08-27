70b262d36b7854dd6d5c312696ca195d466908a0
jooeunhan <yeppijune@gmail.com>
2026-08-27 10:26:20 +0900

    :art: Design: 버튼 디자인 추가

--- src/components/Button/CommonButton.jsx
+++ src/components/Button/CommonButton.jsx
@@ -1,15 +1,36 @@
 import styled from "styled-components";
 
+function CommonButton({
+  children,
+  onClick,
+  type = "button",
+  disabled = false,
+}) {
+  return (
+    <Button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </Button>
+  );
+}
+
+export default CommonButton;
+
 const Button = styled.button`
+  width: 100%;
+  height: 54px;
+
   border: none;
   border-radius: 16px;
 
   background: #73c391;
-
   color: #ffffff;
 
-  font-size: 15px;
-  font-weight: 600;
+  font-size: 14px;
+  font-weight: 700;
 
   cursor: pointer;
 
@@ -18,12 +39,11 @@ const Button = styled.button`
   &:hover {
     background: #63b682;
   }
+
+  &:disabled {
+    background: #d9e5dd;
+    color: #ffffff;
+
+    cursor: default;
+  }
 `;
-
-const CommonButton = ({ children, onClick, ...props }) => (
-  <Button onClick={onClick} {...props}>
-    {children}
-  </Button>
-);
-
-export default CommonButton;
